dd7fab72d1ea0aab02461827cce662b584b54191
Exesting <zadnovaleksej@gmail.com>
2026-02-17 18:40:34 +0300

    Update Main.java

--- Оболочки и Generics 10.02.2026/Main.java
+++ Оболочки и Generics 10.02.2026/Main.java
@@ -2,12 +2,12 @@ public class Main {
 	public static void main(String[] args) {
 		// Сценарий А:
 		ItemStorage<Integer> a = new ItemStorage<>();
-		a.setValue(100); // Кэширование, следовательно a1(100) сравнение 100 это один и тот же объект при сравнении == будет выводить true и при .equals тоже true
+		a.setValue(100); // Кэширование, следовательно a(100) сравнение 100 это один и тот же объект при сравнении == будет выводить true и при .equals тоже true
 		a.compareWith(100);
 
 		// Сценарий Б:
 		ItemStorage<Integer> b = new ItemStorage<>();
-		b.setValue(200); // Тут уже кэширование нет, поэтому значения больше чем 127, являются отдельными объектами, при .equals будет true, при == false
+		b.setValue(200); // Тут уже кэширования нет, поэтому значения больше чем 127, являются отдельными объектами, при .equals будет true, при == false
 		b.compareWith(200);
 
 		//Сценарий В:
@@ -19,4 +19,5 @@ public class Main {
 			System.out.println("Значения идентичны. Контроль качества пройден");
 		}
 	}
+
 }
