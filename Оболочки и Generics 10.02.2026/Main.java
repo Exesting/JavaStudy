42ea0039594c0654641f97925eab65100089fba0
Exesting <zadnovaleksej@gmail.com>
2026-02-17 18:39:31 +0300

    Add files via upload

---
+++ Оболочки и Generics 10.02.2026/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+	public static void main(String[] args) {
+		// Сценарий А:
+		ItemStorage<Integer> a = new ItemStorage<>();
+		a.setValue(100); // Кэширование, следовательно a1(100) сравнение 100 это один и тот же объект при сравнении == будет выводить true и при .equals тоже true
+		a.compareWith(100);
+
+		// Сценарий Б:
+		ItemStorage<Integer> b = new ItemStorage<>();
+		b.setValue(200); // Тут уже кэширование нет, поэтому значения больше чем 127, являются отдельными объектами, при .equals будет true, при == false
+		b.compareWith(200);
+
+		//Сценарий В:
+		ItemStorage<Fraction> c1 = new ItemStorage<>();
+		ItemStorage<Fraction> c2 = new ItemStorage<>();
+		c1.setValue(new Fraction(1, 2));
+		c2.setValue(new Fraction(1, 2)); //тут либо с иквалс, либо с компейрвис, но с переопределенным через обджектс иквалс, а обджектс мы не прошли
+		if (c1.getValue().equals(c2.getValue())) {
+			System.out.println("Значения идентичны. Контроль качества пройден");
+		}
+	}
+}
